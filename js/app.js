7b9ae904d06af67ad45020e778539c6125cc6c11
Steve <sbrennison@gmail.com>
2018-06-29 16:58:14 -0500

    added Readme

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+
+const canvas = document.getElementById('my-canvas');
+// this would be like const canvas = $('my-canvas')
+console.log(canvas);
+console.log(canvas.width);
+console.log(canvas.height);
+
+const ctx = canvas.getContext('2d');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
