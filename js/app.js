53169e0086d9a6cda42aa95e24c30f60407d4a0f
Steve <sbrennison@gmail.com>
2018-07-02 14:28:35 -0500

    added block shapes

--- js/app.js
+++ js/app.js
@@ -11,18 +11,14 @@ const ctx = canvas.getContext('2d');
 
 // Create playing field
 
-// function: draw a square --  commit
-// maybe make Square a class (= you should make a square class)
-
+// Create square
 class Square {
-
   constructor(x, y, width, height) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
   }
-
   draw(){
     ctx.beginPath();
     ctx.rect(this.x, this.y, this.width, this.height);
@@ -30,21 +26,95 @@ class Square {
     ctx.fill();
     ctx.closePath();
 
+  // startGame(){}
   // gameOver(){}
   }
 }
 
-class Piece {
 
-  constructor() {
-    // array of Squares
+// function: draw a piece -- first make it work on its own
+// start thinking about 
+// piece class -- an array for 4 "Squares"
+
+class Block {
+
+  constructor(x, y, shape) {
+    this.x = x; 
+    this.y = y;
+    this.shape = [
+
+      // const oBlock =
+      [[0, 0, 0, 0],
+       [0, 1, 1, 0],
+       [0, 1, 1, 0],
+       [0, 0, 0, 0]],
+    
+      // const iBlock =
+      [[0, 0, 1, 0],
+       [0, 0, 1, 0],
+       [0, 0, 1, 0],
+       [0, 0, 1, 0]],
+    
+      // const tBlock =
+      [[0, 0, 1, 0],
+       [0, 1, 1, 0],
+       [0, 0, 1, 0],
+       [0, 0, 0, 0]],
+    
+      // const sBlock =
+      [[0, 0, 0, 0],
+       [0, 0, 1, 1],
+       [0, 1, 1, 0],
+       [0, 0, 0, 0]],
+    
+      // const zBlock =
+      [[0, 0, 0, 0],
+       [0, 1, 1, 0],
+       [0, 0, 1, 1],
+       [0, 0, 0, 0]],
+    
+      // const jBlock =
+      [[0, 0, 1, 0],
+       [0, 0, 1, 0],
+       [0, 1, 1, 0],
+       [0, 0, 0, 0]],
+    
+      // const lBlock =
+      [[0, 1, 0, 0],
+       [0, 1, 0, 0],
+       [0, 1, 1, 0],
+       [0, 0, 0, 0]]]; 
+
+    if (1 === true){
+      square.draw();
+    } else {
+
+    }
+
+    // if 1, print a square, if 0, do nothing
+    // this.shape.push()
+
+
+    // if able - add uBlock, xBlock, and dotBlock
   }
-
 
   draw(){
+    // square.draw();
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.shape);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.closePath();
   }
 }
-// piece.draw();
+// block.draw();
+// const block = new Block();
+
+
+const square = new Square(270, 0, 30, 30);
+square.draw();
+// randomize block spawn
+
 
 function makeGrid() {
 // horizontal
@@ -65,28 +135,12 @@ function makeGrid() {
   }
 }
 
-const square = new Square(270, 0, 30, 30);
-square.draw();
-
-
-
-
-// function: draw a piece -- first make it work on its own
-
-// start thinking about 
-// piece class -- an array for 4 "Squares"
-// this might mean "square" should be class
-
-
-
 
 function clearCanvas(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-
-
-// Timer
+// // Timer --- must start AFTER clearCanvas
 // let time = 0;
 
 // const startTimer = () => {
@@ -95,8 +149,7 @@ function clearCanvas(){
 
 //   $('#timer').text('timer: ' + time + 's');
 
-//   }, 250);
-
+//   }, 2000);
 
 
 
@@ -104,35 +157,36 @@ function clearCanvas(){
 // make it move -- 
 
 // make it rotate, rotate and other such functionalities you need
-// might be a method of the Piece class?
+// might be a method of the Block class?
 document.addEventListener('keydown', (event) => {
   console.log("pushing key", event.keyCode);
 
   // up 38 - set rotate
   if(event.keyCode === 38 && square.y > 0){
-    square.y -= 10; // may want to use a val much higher than 1
+    square.y -= 30; 
   }
 
   // down 40 - increase speed
                                     // keeps you from going off edge
   if(event.keyCode === 40 && square.y + square.height < canvas.height){
-    square.y += 20;
+    square.y += 30;
   }
 
   // left 37
   if(event.keyCode === 37 && square.x > 0){
-    square.x -= 10;
+    square.x -= 30;
   }
   // right 39
   if(event.keyCode === 39 && square.y > square.y + square.width < canvas.width){
-    square.x += 10;
+    square.x += 30;
   }
-  // Use space bar to auto-drop?
+  // Use space bar to auto-drop or pause?
 
     // clearCanvas();
     // square.draw();
-   
 });
+
+
 
 function animateCanvas() {
 
