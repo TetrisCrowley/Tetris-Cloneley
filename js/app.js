8d115ce8a818cf12c3e44863006bbff59230db52
Steve <sbrennison@gmail.com>
2018-07-05 13:14:02 -0500

    seemingly adapted collision logic that doesn't give an error

--- js/app.js
+++ js/app.js
@@ -11,9 +11,14 @@ const ctx = canvas.getContext('2d');
 
 // Block class -- an array of 4 "Squares"
 
-// Add score, lines cleared, and next piece preview
-// Move timer
-    
+// Add score, lines cleared,
+
+// Extras:
+  // Every 10000 points, increase fall speed slightly
+  // Add bonuses for multiple line clears, see http://tetris.wikia.com/wiki/Scoring
+  // Add sounds and music
+  // Next piece preview
+
 class Block {
 
   constructor(type) {
@@ -118,7 +123,7 @@ class Block {
         for(let j = 0; j < this.shape[i].height; j++) {
           if(this.shape[i][j] === 1 && i < highestJ){
             highestJ = j;
-          }
+          } //else if(this.shape[i][j] === 1 && i < ){}
         }       
       }
       return highestJ * 30;
@@ -160,31 +165,88 @@ class Block {
 // console.log(block.y + block.height, "block");
 // console.log(canvas.height, "canvas");
 // separate to class^^^
+
+
+// This detects if either this or the declared function intersects on any one side
+  contains(x, y) {
+    if (x >= this.x &&
+        x <= this.x + this.width &&
+        y >= this.y &&
+        y <= this.y + this.height) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+  intersects(shape) {
+    if (this.contains(shape.x, shape.y) ||
+        this.contains(shape.x + shape.width, shape.y) ||
+        this.contains(shape.x, shape.y + shape.height) ||
+        this.contains(shape.x + shape.width, shape.y + shape.height)) {
+      return true;
+    } else if (
+        shape.contains(this.x, this.y) ||
+        shape.contains(this.x + this.width, this.y) ||
+        shape.contains(this.x, this.y + this.height) ||
+        shape.contains(this.x + this.width, this.y + this.height)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 }
 
 
-// const game = {
-  // choices: ["I", "J", "L", "T", "S", "Z"]
+
+
+// const Game = {
+  // nextBlockId: (next block we want to create)
+  // newBlock: block instance that's currently moving down the board
+  // currentX: for newBlock
+  // currentY: for newBlock
+  // }
+
+
+  // choices: ["I", "J", "L", "T", "S", "Z"] - how to translate from block class?
   // some sort of crazy (prob 2d array) data structure to track 
   // which squares are filled by previous blocks
+
   // createBlock(){ 
     // choose random letter from above
     // create new block
   // }
 // }
+// currentBlock
 
-let gameOver = false
 
-// Create playing field
+// const gameGrid = 
+ // [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ //  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
 // startGame(){}
-// press space to start
+// press space to start or make button
 
+let gameOver = false
 // gameOver(){}
 // if block reaches canvas.height, clearInterval(IntervalId)
 // prompt(SUCKS TO SUCK)
 
-const block = new Block("Z");
+const block = new Block("T");
 block.draw();
 // randomize block spawn
 // function randomBlock() {
@@ -192,8 +254,28 @@ block.draw();
 // }
 // when block stops, spawn new
 
-// Every 10000 points, increase fall speed slightly
-// Add bonuses for multiple line clears, see http://tetris.wikia.com/wiki/Scoring
+
+
+// // Timer --- must start AFTER clearCanvas
+// add 10 points every second
+// let time = 0;
+
+// startTimer(){
+//     const intervalId = setInterval(() => {
+//     time++;
+//     block.y--;
+//     if block reaches bottom, stop falling. How?
+//     score += 100;
+
+//   $('#timer').text('Timer: ' + time + 's');s
+    // document.getElementById -- that means you ahve an ARRAY
+    // to get 1 element youw ill have to index into the array
+    // USE IDS INSTEAD
+//   }, 2000);
+
+
+
+
 
 
 function makeGrid() {
@@ -229,7 +311,6 @@ document.addEventListener('keydown', (event) => {
   if(event.keyCode === 38){
     block.rotate();
   }
-
   // down 40 - increase speed
                                       // keeps you from going off edge
   if(event.keyCode === 40 && block.y + block.getBottomEdgeYOffset() < canvas.height){
@@ -243,7 +324,6 @@ document.addEventListener('keydown', (event) => {
   if(event.keyCode === 39 && block.x + block.getRightEdgeXOffset() < canvas.width){
     block.x += 30;  
   }
-
   // Use space bar to auto-drop or pause? (key 32)
 });
 
@@ -259,25 +339,9 @@ function animateCanvas() {
 
   // window.cancelAnimationFrame(animate);
     
-  // console.log("derp");
-
   window.requestAnimationFrame(animateCanvas);
 }
 
-// // Timer --- must start AFTER clearCanvas
-// add 10 points every second
-// let time = 0;
-
-// const startTimer = () => {
-//     const intervalId = setInterval(() => {
-//     time++;
-//     score += 100;
-
-//   $('#timer').text('Timer: ' + time + 's');s
-    // document.getElementsByClassName -- that means you ahve an ARRAY
-    // to get 1 element youw ill have to index into the array
-    // USE IDS INSTEAD
-//   }, 2000);
 
 
 
